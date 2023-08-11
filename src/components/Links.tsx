7f163213b27dfc0eae94762eee1130617e5b853c
enes-erenn <thenesern@gmail.com>
2023-08-11 17:47:40 +0300

    Added Slider

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -8,8 +8,8 @@ const Links = ({ handleChange }: Props) => {
   const links = [
     { id: 1, title: "Homepage", url: "/" },
     { id: 2, title: "Menu", url: "/menu" },
-    { id: 3, title: "Working Hours", url: "/" },
-    { id: 4, title: "Contact", url: "/" },
+    { id: 3, title: "Working Hours", url: "/working-hours" },
+    { id: 4, title: "Contact", url: "/contact" },
   ];
 
   return (
