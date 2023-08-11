fcc8fcac1a62dc60426e0a58127178c1c8820f36
enes-erenn <thenesern@gmail.com>
2023-08-11 18:31:22 +0300

    Added Offer

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+const Countdown = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const endingDate = new Date();
+  endingDate.setDate(endingDate.getDate() + 6);
+
+  let difference = +new Date(endingDate.toDateString()) - +new Date();
+  const [delay, setDelay] = useState(difference);
+
+  const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((difference / 1000 / 60) % 60);
+  const s = Math.floor((difference / 1000) % 60);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDelay(delay - 1);
+    }, 1000);
+
+    if (delay === 0) {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <>
+      {isClient ? (
+        <span className="font-bold text-5xl text-yellow-300">
+          {d}:{h}:{m}:{s}
+        </span>
+      ) : (
+        <p>...</p>
+      )}
+    </>
+  );
+};
+
+export default Countdown;
